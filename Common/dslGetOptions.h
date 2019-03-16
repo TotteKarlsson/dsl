fb9f9fa645d21f6b11b7d2b34ac872cd8d22236c
Totte Karlsson <tottek@gmail.com>
2019-03-15 17:15:13 -0700

    Adding option parsing

--- Common/dslGetOptions.h
+++ Common/dslGetOptions.h
@@ -5,9 +5,11 @@
 
 namespace dsl
 {
+
 DSL_COMMON extern int   optind;
 DSL_COMMON extern int   opterr;
 DSL_COMMON extern char  *optarg;
 DSL_COMMON int          getOptions(int argc, char *argv[], char *optstring);
+
 }
 #endif
