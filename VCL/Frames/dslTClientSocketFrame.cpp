498a1e4ad4c9c82f77ddf35ca2ca99abe1fd90fd
Totte Karlsson <totte@dunescientific.com>
2019-10-24 17:08:58 -0700

    Added client socket frame

---
+++ VCL/Frames/dslTClientSocketFrame.cpp
@@ -0,0 +1,218 @@
+#pragma hdrstop
+#include <string>
+#include <vector>
+#include "dslTClientSocketFrame.h"
+#include "dslUtils.h"
+#include "dslVCLUtils.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma link "dslIntEdit"
+#pragma link "TIntegerLabeledEdit"
+#pragma link "dslTIntegerEdit"
+#pragma resource "*.dfm"
+
+using namespace std;
+using namespace dsl;
+
+TClientSocketFrame *ClientSocketFrame;
+
+//---------------------------------------------------------------------------
+__fastcall TClientSocketFrame::TClientSocketFrame(TComponent* Owner)
+    :
+    TFrame(Owner),
+    SB(nullptr),
+    mSocketClient()
+{
+    nrOfMessages = 0;
+    BitBtn1->Caption = "Connect";
+    StatusBar1->Visible = false;
+
+    if(SB)
+    {
+        SB->Panels->Items[0]->Text = "Disconnected...";
+    }
+}
+
+//---------------------------------------------------------------------------
+void __fastcall TClientSocketFrame::SetStatusBar(TStatusBar* sb)
+{
+    StatusBar1->Visible = false;
+    SB = sb;
+}
+
+////ClientSocket stuff
+//void __fastcall TClientSocketFrame::ClientSocketConnect(TObject *Sender,
+//      TCustomWinSocket *Socket)
+//{
+//    ConsoleMemo->Clear();
+//    RecMsgMemo->Clear();
+//    BitBtn1->Caption = "Disconnect";
+//    StatusBar1->Panels->Items[0]->Text = "Connected to: " + Socket->RemoteHost;
+//    nrOfMessages = 0;
+//}
+
+////---------------------------------------------------------------------------
+//void __fastcall TClientSocketFrame::ClientSocketDisconnect(TObject *Sender,
+//      TCustomWinSocket *Socket)
+//{
+//    BitBtn1->Caption = "Connect";
+//    if(SB)
+//        SB->Panels->Items[0]->Text = "Disconnected...";
+//}
+//
+////---------------------------------------------------------------------------
+//void __fastcall TClientSocketFrame::ClientSocketError(TObject *Sender,
+//      TCustomWinSocket *Socket, TErrorEvent ErrorEvent, int &ErrorCode)
+//{
+//  RecMsgMemo->Lines->Add("Error connecting to:" + Server + " on port number " + Sysutils::IntToStr(PortNrE->getValue()) );
+//  RecMsgMemo->Lines->Add("The error code was: " + Sysutils::IntToStr(ErrorCode) );
+//  //switch(ErrorCode)
+////  {
+////    case eeGeneral:         break;
+////    case eeSend:            break;
+////    case eeReceive:         break;
+////    case eeConnect:         break;
+////    case eeDisconnect:      break;
+////    case eeAccept:          break;
+////
+////    default:    break;
+////  }
+//    mSocketClient.close();
+//    ErrorCode = 0;        //Don't raise an exception
+//    BitBtn1->Caption = "Connect";
+//    if(SB)
+//        SB->Panels->Items[0]->Text = "Disconnected...";
+//}
+
+////---------------------------------------------------------------------------
+//void __fastcall TClientSocketFrame::mSocketClientRead(TObject *Sender, TCustomWinSocket *Socket)
+//{
+//    lastMessage = Socket->ReceiveText().c_str();
+//    vector<string> strs;
+//    strs = splitString(lastMessage, "\n");
+//    for(unsigned int i=0; i < strs.size(); i++)
+//    {
+//        if(strs[i].size())
+//            RecMsgMemo->Lines->Add(strs[i].c_str());
+//    }
+//}
+//
+//---------------------------------------------------------------------------
+void __fastcall TClientSocketFrame::ToggleConnectionExecute(TObject *Sender)
+{
+     mSocketClient.isConnected() ? DisconnectExecute(Sender) : ConnectExecute(Sender);
+}
+
+//---------------------------------------------------------------------------
+void __fastcall TClientSocketFrame::DisconnectExecute(TObject *Sender)
+{
+  mSocketClient.disConnect();
+  BitBtn1->Caption = "Connect";
+  if(SB)
+      SB->Panels->Items[0]->Text = "Disconnected...";
+}
+
+//---------------------------------------------------------------------------
+void __fastcall TClientSocketFrame::ConnectExecute(TObject *Sender)
+{
+    if (mSocketClient.isConnected())
+    {
+        return;
+    }
+
+    Server = ServerCB->Text;
+    PortNr = PortNrE->GetNumber();
+    if (Server.Length() < 1)
+    {
+        MessageDlg("Assign a valid server!", mtWarning, TMsgDlgButtons() << mbOK, 0);
+        return;
+    }
+
+    mSocketClient.connect(PortNrE->GetNumber(), stdstr(Server));
+}
+
+//---------------------------------------------------------------------------
+void __fastcall TClientSocketFrame::ConsoleMemoKeyDown(TObject *Sender, WORD &Key, TShiftState Shift)
+{
+    if (Key == VK_RETURN && !Shift.Contains(ssShift))
+    {
+        string msg;
+           for(int i = 0; i < ConsoleMemo->Lines->Count; i++)
+        {
+            msg += stdstr(ConsoleMemo->Lines->Strings[i]);
+        }
+        //Add newline
+        msg += '\n';
+        Send(msg.c_str());
+        ClearConsoleMemoAExecute(Sender);
+      }
+}
+
+int __fastcall TClientSocketFrame::Send(const string& msg)
+{
+    string res = mSocketClient.request(msg, true);
+    RecMsgMemo->Lines->Add(vclstr("Response: " + res));
+    return 0;
+}
+
+//void __fastcall TClientSocketFrame::mSocketClientConnecting(TObject *Sender, TCustomWinSocket *Socket)
+//{
+//    BitBtn1->Caption = "Connecting..";
+//    if(SB)
+//    {
+//        SB->Panels->Items[0]->Text = "Connecting...";
+//    }
+//}
+
+//---------------------------------------------------------------------------
+//void __fastcall TClientSocketFrame::mSocketClientLookup(TObject *Sender, TCustomWinSocket *Socket)
+//{
+//    BitBtn1->Caption = "Connecting..";
+//    if(SB)
+//    {
+//        SB->Panels->Items[0]->Text = "Connecting...";
+//    }
+//}
+
+//---------------------------------------------------------------------------
+void __fastcall TClientSocketFrame::ReconnectTimerTimer(TObject *Sender)
+{
+    //Try to reconnect to server
+    ConnectExecute(Sender);
+}
+
+//---------------------------------------------------------------------------
+void __fastcall TClientSocketFrame::ToggleConnectionUpdate(TObject *Sender)
+{
+    //Code when the connection status changes
+    if(mSocketClient.isConnected())
+    {
+        ReconnectTimer->Enabled = false;    //turn off the reconnect timer
+    }
+    else
+    {
+        AutoReconnectCB->Enabled = true;
+        ReconnectTimer->Enabled = AutoReconnectCB->Checked;
+    }
+}
+
+//---------------------------------------------------------------------------
+void __fastcall TClientSocketFrame::ClearReceivedMemoAExecute(TObject *Sender)
+{
+    RecMsgMemo->Clear();
+}
+
+//---------------------------------------------------------------------------
+void __fastcall TClientSocketFrame::ClearConsoleMemoAExecute(TObject *Sender)
+{
+    ConsoleMemo->Clear();
+}
+
+//---------------------------------------------------------------------------
+void __fastcall TClientSocketFrame::FrameExit(TObject *Sender)
+{
+    SetStatusBar(nullptr);
+}
+
+
+
---
+++ VCL/Frames/dslTClientSocketFrame.dfm
@@ -0,0 +1,182 @@
+object ClientSocketFrame: TClientSocketFrame
+  Left = 0
+  Top = 0
+  Width = 654
+  Height = 543
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  ParentFont = False
+  TabOrder = 0
+  OnExit = FrameExit
+  object Panel2: TPanel
+    Left = 0
+    Top = 0
+    Width = 654
+    Height = 543
+    Align = alClient
+    Caption = 'Panel2'
+    PopupMenu = SocketFramePopupMenu1
+    TabOrder = 0
+    object StatusBar1: TStatusBar
+      Left = 1
+      Top = 523
+      Width = 652
+      Height = 19
+      Panels = <
+        item
+          Text = 'isConnected'
+          Width = 50
+        end>
+    end
+    object ToolBar1: TToolBar
+      Left = 1
+      Top = 1
+      Width = 652
+      Height = 35
+      BorderWidth = 1
+      ButtonHeight = 21
+      Caption = 'ToolBar1'
+      EdgeBorders = [ebTop, ebBottom]
+      TabOrder = 1
+      object ServerCB: TComboBox
+        Left = 0
+        Top = 0
+        Width = 137
+        Height = 21
+        ItemIndex = 0
+        TabOrder = 0
+        Text = 'localhost'
+        Items.Strings = (
+          'localhost'
+          '3.112.212.99')
+      end
+      object BitBtn1: TBitBtn
+        Left = 137
+        Top = 0
+        Width = 79
+        Height = 21
+        Action = ToggleConnection
+        Align = alLeft
+        Caption = 'TogglCon'
+        TabOrder = 2
+      end
+      object AutoReconnectCB: TCheckBox
+        Left = 216
+        Top = 0
+        Width = 128
+        Height = 21
+        BiDiMode = bdLeftToRight
+        Caption = 'Auto Reconnect'
+        ParentBiDiMode = False
+        TabOrder = 1
+      end
+      object PortNrE: TIntegerEdit
+        Left = 344
+        Top = 0
+        Width = 121
+        Height = 21
+        NumbersOnly = True
+        TabOrder = 3
+        Text = '0'
+      end
+    end
+    object Panel1: TPanel
+      Left = 1
+      Top = 36
+      Width = 652
+      Height = 487
+      Align = alClient
+      Caption = 'Panel1'
+      TabOrder = 2
+      object Splitter1: TSplitter
+        Left = 1
+        Top = 372
+        Width = 650
+        Height = 7
+        Cursor = crVSplit
+        Align = alBottom
+        ExplicitTop = 85
+        ExplicitWidth = 383
+      end
+      object RecMsgMemo: TMemo
+        Left = 1
+        Top = 1
+        Width = 650
+        Height = 371
+        Align = alClient
+        Color = clBtnFace
+        Lines.Strings = (
+          '')
+        ReadOnly = True
+        ScrollBars = ssBoth
+        TabOrder = 0
+        WordWrap = False
+      end
+      object ConsoleMemo: TMemo
+        Left = 1
+        Top = 379
+        Width = 650
+        Height = 107
+        Align = alBottom
+        PopupMenu = ConsolePopup
+        ScrollBars = ssVertical
+        TabOrder = 1
+        WordWrap = False
+        OnKeyDown = ConsoleMemoKeyDown
+      end
+    end
+  end
+  object SocketFramePopupMenu1: TPopupMenu
+    Left = 80
+    Top = 56
+    object Connect1: TMenuItem
+      Action = ClearReceivedMemoA
+    end
+  end
+  object SocketFrameActionList1: TActionList
+    Left = 160
+    Top = 144
+    object ToggleConnection: TAction
+      Category = 'socketStuff'
+      Caption = 'ToggleConnection'
+      OnExecute = ToggleConnectionExecute
+      OnUpdate = ToggleConnectionUpdate
+    end
+    object ClearReceivedMemoA: TAction
+      Category = 'MemosA'
+      Caption = 'Clear'
+      OnExecute = ClearReceivedMemoAExecute
+    end
+    object Disconnect: TAction
+      Category = 'socketStuff'
+      Caption = 'Disconnect'
+      OnExecute = DisconnectExecute
+    end
+    object Connect: TAction
+      Category = 'socketStuff'
+      Caption = 'Connect'
+      OnExecute = ConnectExecute
+    end
+    object ClearConsoleMemoA: TAction
+      Category = 'MemosA'
+      Caption = 'Clear'
+      OnExecute = ClearConsoleMemoAExecute
+    end
+  end
+  object ReconnectTimer: TTimer
+    Enabled = False
+    OnTimer = ReconnectTimerTimer
+    Left = 264
+    Top = 72
+  end
+  object ConsolePopup: TPopupMenu
+    Left = 208
+    Top = 424
+    object Clear1: TMenuItem
+      Action = ClearConsoleMemoA
+    end
+  end
+end
